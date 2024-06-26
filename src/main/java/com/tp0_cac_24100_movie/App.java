c7cce562151f14f1756ad1765efdbf820bc47329
BlackJhossy <jhossymarbalderrama@gmail.com>
2024-06-26 13:28:16 -0300

    Upload Project Github

---
+++ src/main/java/com/tp0_cac_24100_movie/App.java
@@ -0,0 +1,13 @@
+package com.tp0_cac_24100_movie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/tp0_cac_24100_movie/AppTest.java
@@ -0,0 +1,20 @@
+package com.tp0_cac_24100_movie;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
